ea22d8c65952c89d0f0f685c80c62a4543ba1f90
shrubycow <alexandriyskiy0@gmail.com>
2020-04-12 15:36:22 +0300

    db settings

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -117,3 +117,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
 django_heroku.settings(locals())
+
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config() }

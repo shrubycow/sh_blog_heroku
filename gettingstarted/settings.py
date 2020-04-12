87966f65ce0240f26708ee34289add12fbf8d0f1
shrubycow <alexandriyskiy0@gmail.com>
2020-04-12 15:47:21 +0300

    db

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import django_heroku
-import dj_database_url
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -84,8 +83,6 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators

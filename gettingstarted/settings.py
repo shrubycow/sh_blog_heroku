0ac321bf21e27b855464f45120822c47ea913f6d
shrubycow <alexandriyskiy0@gmail.com>
2020-04-20 18:49:34 +0300

    settings

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -120,10 +120,6 @@ USE_TZ = False
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 

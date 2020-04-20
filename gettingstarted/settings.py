d1622b4815d07e8c42e23c15b15f763fc3ead326
shrubycow <alexandriyskiy0@gmail.com>
2020-04-20 17:14:21 +0300

    try to fix static problem

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -117,7 +117,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = "/static/"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

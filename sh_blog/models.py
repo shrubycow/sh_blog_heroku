d4620145a383e09d2661652c32784210ecb2d80d
shrubycow <alexandriyskiy0@gmail.com>
2020-04-29 13:04:00 +0300

    make possible set post image null

--- sh_blog/models.py
+++ sh_blog/models.py
@@ -25,7 +25,7 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     fixed = models.BooleanField(default=False)
-    image = models.CharField(max_length=250)
+    image = models.CharField(max_length=250, blank=True, null=True)
 
     class Meta:
         ordering = ('-publish',)
